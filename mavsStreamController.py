56c11012ee36c6c80cc2ff48e99cdcee34cdde90
AshHuston <dsguyhuston@gmail.com>
2024-06-20 08:45:27 -0400

    psuedocode

---
+++ mavsStreamController.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import os
+
+# tk.buildTheGui()
+    # Player 1 Life
+    # Player 2 Life
+    # Player 1 Name
+    # Player 2 Name
+    # Player 1 Deck
+    # Player 2 Deck
+    # Player 1 game wins
+    # Player 2 game wins
+    # display cardname
+
+
+# open, read, and close the files
+    # Above variables 
+    # Combined gamewins string
+
+# Write values to the gui ('saving' from previous launch)
+
+while (True):
+# read the gui 
+
+# if gui and files are missmatched:
+    # Open appropriate file(s), write new gui values to files, reassign  file variables, then close file(s).
+
+# wait .5 seconds
+
+   pass
+
+
